4e25de3e305afd34d60cebf0f4fd3c1e63ded94e
SittikronNon <sittikron.lamulsab@gmail.com>
2023-01-11 19:00:52 +0700

    some sound

--- Assets/Script/ButtonBehaviour.cs
+++ Assets/Script/ButtonBehaviour.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonBehaviour : MonoBehaviour
 {
     public TextMeshProUGUI myText; 
     // Start is called before the first frame update
-    public void OnButtonPressed()
+    public void MoveToScene(int sceneID)
     {
-        myText.text = "Button was pressed";
+        SceneManager.LoadScene(sceneID);
     }
+    
 }
